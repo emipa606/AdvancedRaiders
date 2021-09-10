8df354c1ef13c06d457075e49f38d15fdd2e6078
Daniil <silverelitem@yandex.ru>
2021-09-10 10:28:56 +0300

    Initial commit literally nothing is working rn

---
+++ Source/AdvancedRaiders/HediffDefOf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace AdvancedRaiders
+{
+    [DefOf]
+    public static class HediffDefOf
+    {
+        public static HediffDef OmegaStimulant;
+
+        static HediffDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(HediffDefOf));
+    }
+}
---
+++ Source/AdvancedRaiders/Toils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse.AI;
+namespace AdvancedRaiders
+{
+    public static class Toils
+    {
+        public static Toil FindDownedAllyAndInjectStimulant(
+            TargetIndex targetInd,)
+        {
+
+        }
+    }
+}
